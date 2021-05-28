df71b61467e6e971dc83e5a46b48743fbcbdf4f1
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-05-28 10:53:48 +0100

    assigned 0 to toal

--- katas/square_add_num.py
+++ katas/square_add_num.py
@@ -5,9 +5,10 @@
 
 # add up the numbers in that list
 
-
+# method 2
 def sq(numbers):
     sq_list = []
+    total = 0
     for n in numbers:
         sq_num = pow(n, 2)
         sq_list.append(sq_num)
@@ -16,3 +17,17 @@ def sq(numbers):
 
 
 sq([1, 2, 3])
+
+
+# method 1
+def square_sum(numbers):
+    array_of_num = []
+    added = 0
+    for n in numbers:
+        sq = n**2
+        array_of_num.append(sq)
+        added = sum(array_of_num)
+    print(added)
+
+
+square_sum([])
