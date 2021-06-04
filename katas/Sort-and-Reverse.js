645c1bd74ff17bd073bc9a91b7824c03712d5968
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-06-04 14:41:06 +0100

    fixed function to return number not string

--- katas/Sort-and-Reverse.js
+++ katas/Sort-and-Reverse.js
@@ -1,15 +1,20 @@
+/**
+ *
+ * @param {number} num
+ * @returns {number}
+ */
 function reverseNum(num) {
   const num1 = String(num).split("");
   num1.sort().reverse();
-  const backToNum = num1.join("");
+  const backToNum = Number(num1.join(""));
   return backToNum;
 }
 
-console.log(reverseNum(11051998));
+console.log(typeof reverseNum(11051998));
 
-function reverseAgain(num) {
-  result = String(num).split("").sort().reverse().join("");
-  return result;
-}
+// function reverseAgain(num) {
+//   result = String(num).split("").sort().reverse().join("");
+//   return result;
+// }
 
-console.log(reverseAgain(11051998));
+// console.log(reverseAgain(11051998));
